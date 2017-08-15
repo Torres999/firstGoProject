971c67659849f9d61e9318867711b2ff0446462a
lill <dalintorres@icloud.com>
2017-08-15 11:13:12 +0800

    add read database while unknow cloumn name

--- demo/src/database/main.go
+++ demo/src/database/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/mysqlerr"
+	_ "github.com/mysqlerr"
 	"log"
+	"reflect"
 )
 
 var (
@@ -13,29 +17,100 @@ var (
 )
 
 /*
-sql.DB不是一个连接，它是数据库的抽象接口。它可以根据driver打开关闭数据库连接，管理连接池。正在使用的连接被标记为繁忙，
-用完后回到连接池等待下次使用。所以，如果你没有把连接释放回连接池，会导致过多连接使系统资源耗尽。
+	sql.DB不是一个连接，它是数据库的抽象接口。它可以根据driver打开关闭数据库连接，管理连接池。正在使用的连接被标记为繁忙，
+	用完后回到连接池等待下次使用。所以，如果你没有把连接释放回连接池，会导致过多连接使系统资源耗尽。
 
-sql.DB的设计就是用来作为长连接使用的。不要频繁Open, Close。比较好的做法是，为每个不同的datastore建一个DB对象，
-保持这些对象Open。如果需要短连接，那么把DB作为参数传入function，而不要在function中Open, Close。
+	sql.DB的设计就是用来作为长连接使用的。不要频繁Open, Close。比较好的做法是，为每个不同的datastore建一个DB对象，
+	保持这些对象Open。如果需要短连接，那么把DB作为参数传入function，而不要在function中Open, Close。
+*/
+/**
+关于连接池
+	避免错误操作，例如LOCK TABLE后用 INSERT会死锁，因为两个操作不是同一个连接，insert的连接没有table lock。
+	当需要连接，且连接池中没有可用连接时，新的连接就会被创建。
+	默认没有连接上限，你可以设置一个，但这可能会导致数据库产生错误“too many connections”
+	db.SetMaxIdleConns(N)设置最大空闲连接数
+	db.SetMaxOpenConns(N)设置最大打开连接数
+	长时间保持空闲连接可能会导致db timeout
 */
 func main() {
 	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/lilinlin")
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		if driverErr, ok := err.(*mysql.MySQLError); ok {
+			if driverErr.Number == mysqlerr.ER_ACCESS_DENIED_ERROR {
+				log.Fatal("Handle the permission-denied error")
+			} else if driverErr.Number == mysqlerr.ER_NO_DB_ERROR {
+				log.Fatal("Handle the no this db error")
+			} else if driverErr.Number == mysqlerr.ER_BAD_DB_ERROR {
+				log.Fatal("Handle the Unknown database error")
+			} else if driverErr.Number == 44702 {
+				log.Fatal("Handle 44702 error")
+			}
+		}
 	}
 
 	//update(db)
 
 	//updateInTransaction(db)
 
-	readData(db)
+	//readData(db)
 
-	readSingleData(db)
+	readUnknowColumnSize(db)
+
+	//test(db)
+
+	//readSingleData(db)
 
 	defer db.Close()
 }
 
+func test(db *sql.DB) {
+	rows, err := db.Query("select id, last_name, address from author where id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cols, err := rows.Columns() // Remember to check err afterwards
+	values := make([]sql.RawBytes, len(cols))
+	for i, col := range values {
+		fmt.Println(cols[i], string(col))
+	}
+
+	fmt.Println("-----------")
+
+	vals := make([]interface{}, len(cols))
+	//vals := make([][]byte, len(cols))
+	for i, _ := range cols {
+		vals[i] = new(sql.RawBytes)
+	}
+
+	for rows.Next() {
+		err = rows.Scan(vals...) //vals...表示把其作为不定参数传入
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for index, value := range vals {
+			tmp := value.([]byte)
+			fmt.Print("index:", index)
+			//fmt.Println(" value:", value)
+
+			if value == nil {
+				value = "NULL"
+			} else {
+				fmt.Println(" type:", reflect.TypeOf(value))
+				value = string(tmp)
+			}
+			//fmt.Println(" value:", value)
+
+		}
+	}
+}
+
+/**
+read数据
+*/
 func readData(db *sql.DB) {
 	/*如果id为1的不存在，err为sql.ErrNoRows
 	    if err != nil {
@@ -46,7 +121,7 @@ func readData(db *sql.DB) {
 			}
 		}
 	*/
-	rows, err := db.Query("select id, last_name from author where id < ?", 14)
+	rows, err := db.Query("select id, last_name, address from author where id < ?", 14)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,12 +153,20 @@ func readData(db *sql.DB) {
 
 	*/
 	for rows.Next() {
-		//get Data
-		err := rows.Scan(&id, &last_name)
+		var address sql.NullString
+
+		err := rows.Scan(&id, &last_name, &address)
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, last_name)
+
+		// Valid is true if String is not NULL
+		if !address.Valid {
+			fmt.Printf("address is null and id=%v last_name=%v\n", id, last_name)
+		} else {
+			fmt.Println(id, last_name)
+		}
 	}
 	err = rows.Err()
 	if err != nil {
@@ -91,7 +174,10 @@ func readData(db *sql.DB) {
 	}
 }
 
-func readSingleData(db *sql.DB)  {
+/**
+读取单条数据
+*/
+func readSingleData(db *sql.DB) {
 	var name string
 	err := db.QueryRow("select last_name as name from author where id = ?", 1).Scan(&name)
 	if err != nil {
@@ -101,6 +187,59 @@ func readSingleData(db *sql.DB)  {
 
 }
 
+/**
+读取未知列数的数据
+*/
+func readUnknowColumnSize(db *sql.DB) {
+	rows, err := db.Query("select id, last_name, address from author where id < ?", 6)
+	if err != nil {
+		fmt.Println("Failed to run query", err)
+		return
+	}
+
+	cols, err := rows.Columns()
+	if err != nil {
+		fmt.Println("Failed to get columns", err)
+		return
+	}
+
+	columnNameResult := make([]string, len(cols))
+	for i := 0; i < len(cols); i++ {
+		columnNameResult[i] = cols[i]
+	}
+	fmt.Printf("%v\n", columnNameResult)
+
+	// Result is your slice string.
+	rawResult := make([][]byte, len(cols))
+	dest := make([]interface{}, len(cols)) // A temporary interface{} slice
+	for i, _ := range rawResult {
+		dest[i] = &rawResult[i] // Put pointers to each string in the interface slice；因为rows.scan必须操作一个interface{}的对象
+	}
+
+	result := make([]string, len(cols))
+	for rows.Next() {
+		err = rows.Scan(dest...) //vals...表示把其作为不定参数传入
+		if err != nil {
+			fmt.Println("Failed to scan row", err)
+			return
+		}
+
+		for i, raw := range rawResult {
+			if raw == nil {
+				result[i] = "Null"
+			} else {
+				result[i] = string(raw)
+			}
+		}
+
+		fmt.Printf("%#v\n", result)
+	}
+
+}
+
+/**
+更新数据
+*/
 func update(db *sql.DB) {
 	stmt, err := db.Prepare("INSERT INTO author(first_name,last_name) VALUES(?,?)")
 	if err != nil {
